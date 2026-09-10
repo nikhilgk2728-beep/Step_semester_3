f4c9657b59070d0b548b09daf280eba9f51da5ae
Nikhil <nkhilgk2728@gmail.com>
2026-09-10 22:34:35 +0530

    Add Problem3 get bmi status

---
+++ Problem3W1P1.java
@@ -0,0 +1,48 @@
+public class Problem3W1P1 {
+
+    static String getBmiStatus(double bmi) {
+
+        if (bmi < 18.5) {
+            return "Underweight";
+        }
+        else if (bmi < 25) {
+            return "Normal";
+        }
+        else if (bmi < 30) {
+            return "Overweight";
+        }
+        else {
+            return "Obese";
+        }
+    }
+
+    static void printWellnessReport(double[] heights, double[] weights) {
+
+        System.out.println("Person\tHeight\tWeight\tBMI\tStatus");
+
+        for (int i = 0; i < heights.length; i++) {
+
+            double bmi = weights[i] /
+                         (heights[i] * heights[i]);
+
+            String status = getBmiStatus(bmi);
+
+            System.out.printf(
+                "%d\t%.2f\t%.2f\t%.2f\t%s%n",
+                i + 1,
+                heights[i],
+                weights[i],
+                bmi,
+                status
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        double[] heights = {1.75, 1.60, 1.80};
+        double[] weights = {70, 90, 80};
+
+        printWellnessReport(heights, weights);
+    }
+}
