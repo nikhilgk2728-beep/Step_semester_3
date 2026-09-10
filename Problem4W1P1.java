e0a0d83dc878cbf3ae776ef7b27e6b0c84348d46
Nikhil <nkhilgk2728@gmail.com>
2026-09-10 22:38:00 +0530

    Add Problem4 finding the non repeating character

---
+++ Problem4W1P1.java
@@ -0,0 +1,43 @@
+public class Problem4W1P1 {
+
+    static char findFirstNonRepeatingChar(String text) {
+
+        int[] frequency = new int[256];
+
+        // Count frequency
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            frequency[ch]++;
+        }
+
+        // Find first non-repeating character
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            if (frequency[ch] == 1) {
+                return ch;
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+
+        String text = "swiss";
+
+        char result = findFirstNonRepeatingChar(text);
+
+        if (result == '\0') {
+            System.out.println("No Non-Repeating Character Found");
+        }
+        else {
+            System.out.println(
+                "First Non-Repeating Character: '" + result + "'"
+            );
+        }
+    }
+}
