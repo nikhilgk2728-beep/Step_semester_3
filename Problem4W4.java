e46ba891525a03c3a13fb1e51b5937b0c4d11fd6
Nikhil <nkhilgk2728@gmail.com>
2026-09-10 23:24:54 +0530

    Add Problem4 addition of sub array

---
+++ Problem4W4.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+public class Problem4W4 {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Empty prefix sum
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            currentSum = currentSum + num;
+
+            int required = currentSum - k;
+
+            if (map.containsKey(required)) {
+
+                count = count + map.get(required);
+            }
+
+            map.put(
+                currentSum,
+                map.getOrDefault(currentSum, 0) + 1
+            );
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
