f4f3c83f622efca4652199a267c3b6f5536f0409
Nikhil <nkhilgk2728@gmail.com>
2026-09-10 23:02:33 +0530

    Add Problem1 counting vowels and consonants

---
+++ Problem1W2P2.java
@@ -0,0 +1,35 @@
+public class Problem1W2P2 {
+
+    static void countVowelsAndConsonants(String text) {
+
+        int vowels = 0;
+        int consonants = 0;
+
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u') {
+
+                vowels++;
+            }
+            else if (ch >= 'a' && ch <= 'z') {
+
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels
+                + " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        countVowelsAndConsonants(text);
+    }
+}
